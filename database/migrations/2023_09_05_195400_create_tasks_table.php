32fca80a9722f58ec00963ab7e6899f90cc4d9a7
ShuuDarkin2002 <gomedouglas200@gmail.com>
2023-09-05 18:43:24 -0300

    to-do first Check

---
+++ database/migrations/2023_09_05_195400_create_tasks_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use App\Models\User;
+use App\Models\category;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('description');
+            $table->dateTime('due_date');
+            $table->foreignId(user::class)->references('id')->on('users')->onDelete('CASCADE');
+            $table->foreignidFor(category::class)->references('id')->on('categories')->onDelete('CASCADE');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('tasks', function(Blueprint $table){
+            $table->dropForeignIdFor(user::class);
+            $table->dropForeignIdFor(category::class);
+        });
+        Schema::dropIfExists('tasks');
+    }
+};
